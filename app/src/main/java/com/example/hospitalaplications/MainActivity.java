19304c92e0721067b936b7fc2383954fdf937fce
SIWE0009 <simon.westerdahl0009@stud.hkr.se>
2019-04-07 10:03:15 +0200

    SUCKA DICK OSKAAA

--- app/src/main/java/com/example/hospitalaplications/MainActivity.java
+++ app/src/main/java/com/example/hospitalaplications/MainActivity.java
@@ -5,14 +5,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.hospitalaplications.text_detection.TextRecognition;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -26,6 +27,7 @@ import com.otaliastudios.cameraview.BitmapCallback;
 import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraOptions;
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.FileCallback;
 import com.otaliastudios.cameraview.Frame;
 import com.otaliastudios.cameraview.FrameProcessor;
 import com.otaliastudios.cameraview.Gesture;
@@ -34,6 +36,7 @@ import com.otaliastudios.cameraview.Mode;
 import com.otaliastudios.cameraview.PictureResult;
 import com.otaliastudios.cameraview.VideoResult;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private CameraView cameraView;
     private TextView textView;
     private BitmapCallback bitmapCallback;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textview);
         cameraView = findViewById(R.id.camera);
+        imageView = findViewById(R.id.imageView);
         cameraView.setAudio(Audio.OFF);
         if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, 321321);
         } else {
+            cameraView.setMode(Mode.PICTURE);
+
             cameraView.setLifecycleOwner(this);
             cameraView.addCameraListener(cameraListener);
             //cameraView.addFrameProcessor(frameProcessor);
@@ -70,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private CameraListener cameraListener = new CameraListener() {
+
         @Override
         public void onCameraOpened(@NonNull CameraOptions options) {
             super.onCameraOpened(options);
@@ -77,9 +85,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPictureTaken(@NonNull PictureResult result) {
-            System.out.println("1111111111111111111111111");
-            TextRecognition tr = new TextRecognition(result.getData(), 1);
-            runTextRecognition(tr.getImage());
+            super.onPictureTaken(result);
+            result.toBitmap(1000, 1000, new BitmapCallback() {
+                @Override
+                public void onBitmapReady(@Nullable Bitmap bitmap) {
+                    FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+                    runTextRecognition(image);
+                }
+            });
         }
 
         @Override
@@ -96,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
     private void runTextRecognition(FirebaseVisionImage image) {
         FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
 
-        final Task<FirebaseVisionText> results = textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+        Task<FirebaseVisionText> results = textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
             public void onSuccess(FirebaseVisionText firebaseVisionText) {
                 processTextRecognition(firebaseVisionText);
@@ -110,21 +123,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void processTextRecognition(FirebaseVisionText text) {
-        System.out.println("IS THIS OKAY: " + text);
-        System.out.println("IS THIS OKAY nr 2: " + text.getText());
-        System.out.println("IS THIS OKAY nr 3: " + text.getTextBlocks() + " AND SIZE IS : " + text.getTextBlocks().size());
+
         List<FirebaseVisionText.TextBlock> textBlocks = text.getTextBlocks();
         if(textBlocks.size() == 0) {
             Toast.makeText(this, "No text found", Toast.LENGTH_SHORT).show();
         }
-        System.out.println("THIS IS ALL TEXT: " + text.getText());
         for(FirebaseVisionText.TextBlock textBlock : textBlocks) {
             List<FirebaseVisionText.Line> lines = textBlock.getLines();
             for(FirebaseVisionText.Line line : lines) {
                 List<FirebaseVisionText.Element> elements = line.getElements();
-                for(FirebaseVisionText.Element element : elements) {
-                    System.out.println("THIS IS EVERY ELEMENT AFTER 1 CLICK: " + element);
-                }
+                textView.append(line.getText() + "\n");
             }
         }
     }
